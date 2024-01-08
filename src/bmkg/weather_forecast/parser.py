75103efe866dd94d1bbaf69b515c464b8c35bcf7
Kira <kira68784@gmail.com>
2024-01-08 17:00:47 +0800

    update weather forecast parser
    when area type is sea, there is no any weather information, hence just
    return empty list of weather is area type is not land or sea

--- src/bmkg/weather_forecast/parser.py
+++ src/bmkg/weather_forecast/parser.py
@@ -283,60 +283,74 @@ def parse_weather_forecast_data(
     areas = {}
     for area_element in forecast_element.iterfind("area"):
         area = parse_area_element(area_element)
-
-        parameters: dict[str, WeatherForecastParameter] = {}
-        for parameter_element in area_element.iterfind("parameter"):
-            parameter_id = parameter_element.get("id")
-            if parameter_id is None:
-                raise WeatherForecastParseError(
-                    "id attribute in parameter tag not found"
-                )
-
-            parameter: WeatherForecastParameter
-            if (
-                parameter_id == "hu"
-                or parameter_id == "humax"
-                or parameter_id == "humin"
-            ):
-                parameter = parse_humidity_element(parameter_element)
-            elif (
-                parameter_id == "t" or parameter_id == "tmax" or parameter_id == "tmin"
-            ):
-                parameter = parse_temperature_element(parameter_element)
-            elif parameter_id == "weather":
-                parameter = parse_weather_element(parameter_element)
-            elif parameter_id == "wd":
-                parameter = parse_wind_direction_element(parameter_element)
-            else:
-                # parameter_id == "ws":
-                parameter = parse_wind_speed_element(parameter_element)
-
-                # Choose one of parameter tag that has datetime attribute with
-                # type hourly. Strictly speaking it is timerange tag.
-                parameters["datetime"] = parse_datetime_element(parameter_element)
-
-            parameters[parameter_id] = parameter
-
-        # The length of the data to be zipped is 12. This is because the weather
-        # forecast data received is for three days, each receiving 4 data. However, for
-        # tmin, tmax, hmin, and hmax data, these four data are only given 1 each for
-        # that day, therefore there are only 3 data for 3 days from tmin, tmax, hmin,
-        # and hmax. Because the zip() function will stop at the smallest data length,
-        # therefore we need to multiply the 3 data by 4 to get 12
         weathers = []
-        for dt, weather, t, tmix, tmax, h, hmin, hmax, wd, ws in zip(
-            parameters["datetime"],
-            parameters["weather"],
-            parameters["t"],
-            chain.from_iterable([(val, val, val, val) for val in parameters["tmin"]]),
-            chain.from_iterable([(val, val, val, val) for val in parameters["tmax"]]),
-            parameters["hu"],
-            chain.from_iterable([(val, val, val, val) for val in parameters["humin"]]),
-            chain.from_iterable([(val, val, val, val) for val in parameters["humax"]]),
-            parameters["wd"],
-            parameters["ws"],
-        ):
-            weathers.append(Weather(dt, weather, t, tmix, tmax, h, hmin, hmax, wd, ws))
+
+        # Only land that has weather, sea doesn't
+        if area.type == Type.LAND:
+            parameters: dict[str, WeatherForecastParameter] = {}
+            for parameter_element in area_element.iterfind("parameter"):
+                parameter_id = parameter_element.get("id")
+                if parameter_id is None:
+                    raise WeatherForecastParseError(
+                        "id attribute in parameter tag not found"
+                    )
+
+                parameter: WeatherForecastParameter
+                if (
+                    parameter_id == "hu"
+                    or parameter_id == "humax"
+                    or parameter_id == "humin"
+                ):
+                    parameter = parse_humidity_element(parameter_element)
+                elif (
+                    parameter_id == "t"
+                    or parameter_id == "tmax"
+                    or parameter_id == "tmin"
+                ):
+                    parameter = parse_temperature_element(parameter_element)
+                elif parameter_id == "weather":
+                    parameter = parse_weather_element(parameter_element)
+                elif parameter_id == "wd":
+                    parameter = parse_wind_direction_element(parameter_element)
+                else:
+                    # parameter_id == "ws":
+                    parameter = parse_wind_speed_element(parameter_element)
+
+                    # Choose one of parameter tag that has datetime attribute with
+                    # type hourly. Strictly speaking it is timerange tag.
+                    parameters["datetime"] = parse_datetime_element(parameter_element)
+
+                parameters[parameter_id] = parameter
+
+            # The length of the data to be zipped is 12. This is because the weather
+            # forecast data received is for three days, each receiving 4 data. However,
+            # for tmin, tmax, hmin, and hmax data, these four data are only given 1 each
+            # for that day, therefore there are only 3 data for 3 days from tmin, tmax,
+            # hmin, and hmax. Because the zip() function will stop at the smallest data
+            # length, therefore we need to multiply the 3 data by 4 to get 12
+            for dt, weather, t, tmix, tmax, h, hmin, hmax, wd, ws in zip(
+                parameters["datetime"],
+                parameters["weather"],
+                parameters["t"],
+                chain.from_iterable(
+                    [(val, val, val, val) for val in parameters["tmin"]]
+                ),
+                chain.from_iterable(
+                    [(val, val, val, val) for val in parameters["tmax"]]
+                ),
+                parameters["hu"],
+                chain.from_iterable(
+                    [(val, val, val, val) for val in parameters["humin"]]
+                ),
+                chain.from_iterable(
+                    [(val, val, val, val) for val in parameters["humax"]]
+                ),
+                parameters["wd"],
+                parameters["ws"],
+            ):
+                weathers.append(
+                    Weather(dt, weather, t, tmix, tmax, h, hmin, hmax, wd, ws)
+                )
 
         areas[area] = weathers
 
