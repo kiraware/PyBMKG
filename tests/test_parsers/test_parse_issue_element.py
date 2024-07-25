68fcd2febbb9871085f9de339ec3f9fb4d89b8e1
Kira <117554978+kiraware@users.noreply.github.com>
2024-07-25 19:14:02 +0800

    update timestamp parser in issue tag
    currently, we use timestamp tag in issue tag to get the timestamp and
    convert it to datetime. but we found in test, that it is possible that
    timestamp tag is missing, then raise error. in order to fix it, we use
    another tag which are `year`, `month`, `day`, `hour`, `minute`, and
    `second`.  these tags contain exactly the same value in timestamp tag.
    eventhough it makes the parsing more complex, it allow the flexible API
    wrapper.

--- tests/test_parsers/test_parse_issue_element.py
+++ tests/test_parsers/test_parse_issue_element.py
@@ -6,23 +6,42 @@ from bmkg.exceptions import WeatherForecastParseError
 from bmkg.parsers import parse_issue_element
 
 
-def test_parse_element_with_invalid_attribute():
+@pytest.mark.parametrize(
+    "tag, err_msg",
+    (
+        ("year", "year tag in issue tag not found"),
+        ("month", "month tag in issue tag not found"),
+        ("day", "day tag in issue tag not found"),
+        ("hour", "hour tag in issue tag not found"),
+        ("minute", "minute tag in issue tag not found"),
+        ("second", "second tag in issue tag not found"),
+    ),
+)
+def test_parse_element_with_invalid_attribute(tag, err_msg):
     element = MagicMock()
-    element.find.return_value = None
+    element.find.side_effect = lambda x: None if x == tag else MagicMock()
 
-    with pytest.raises(
-        WeatherForecastParseError, match="timestamp tag in issue tag not found"
-    ):
+    with pytest.raises(WeatherForecastParseError, match=err_msg):
         parse_issue_element(element)
 
 
-def test_parse_element_with_invalid_timestamp_text():
+@pytest.mark.parametrize(
+    "tag, err_msg",
+    (
+        ("year", "year tag in issue tag has no text"),
+        ("month", "month tag in issue tag has no text"),
+        ("day", "day tag in issue tag has no text"),
+        ("hour", "hour tag in issue tag has no text"),
+        ("minute", "minute tag in issue tag has no text"),
+        ("second", "second tag in issue tag has no text"),
+    ),
+)
+def test_parse_element_with_invalid_timestamp_text(tag, err_msg):
     timestamp = MagicMock()
     timestamp.text = None
 
     element = MagicMock()
-    element.find.return_value = timestamp
-    with pytest.raises(
-        WeatherForecastParseError, match="timestamp tag in issue tag has no text"
-    ):
+    element.find.side_effect = lambda x: timestamp if x == tag else MagicMock()
+
+    with pytest.raises(WeatherForecastParseError, match=err_msg):
         parse_issue_element(element)
