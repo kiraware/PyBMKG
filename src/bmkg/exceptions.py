858eaf3e557b7e136a6205497778d6a5a8f71d07
Kira <117554978+kiraware@users.noreply.github.com>
2024-12-29 17:22:12 +0700

    remove unusede exception

--- src/bmkg/exceptions.py
+++ src/bmkg/exceptions.py
@@ -1,20 +1,11 @@
 __all__ = [
     "BMKGError",
-    "WeatherForecastParseError",
 ]
 
 
 class BMKGError(Exception):
     """
     General BMKG API exception.
-    """
-
-    pass
-
-
-class WeatherForecastParseError(BMKGError):
-    """
-    Weather forecast parse exception.
     """
 
     pass
