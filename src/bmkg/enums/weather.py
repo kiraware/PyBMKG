eef11eb3fab474b3ac22eeed7f32c9e5ef1d0f42
Kira <117554978+kiraware@users.noreply.github.com>
2025-01-02 12:13:22 +0700

    fix(lint): fix mypy class attribute and docstring type in enums

--- src/bmkg/enums/weather.py
+++ src/bmkg/enums/weather.py
@@ -12,20 +12,20 @@ class Weather(IntEnum):
     An int enum class that define valid weather.
 
     Attributes:
-        CLEAR_SKIES: `0`
-        SUNNY: `1`
-        PARTLY_CLOUDY: `2`
-        MOSTLY_CLOUDY: `3`
-        OVERCAST: `4`
-        HAZE: `5`
-        SMOKE: `10`
-        THUNDER: `17`
-        FOG: `45`
-        LIGHT_RAIN: `61`
-        MODERATE_RAIN: `63`
-        ISOLATED_SHOWER: `80`
-        THUNDERSTORM: `95`
-        SEVERE_THUNDERSTORM: `97`
+        CLEAR_SKIES (int): `0`
+        SUNNY (int): `1`
+        PARTLY_CLOUDY (int): `2`
+        MOSTLY_CLOUDY (int): `3`
+        OVERCAST (int): `4`
+        HAZE (int): `5`
+        SMOKE (int): `10`
+        THUNDER (int): `17`
+        FOG (int): `45`
+        LIGHT_RAIN (int): `61`
+        MODERATE_RAIN (int): `63`
+        ISOLATED_SHOWER (int): `80`
+        THUNDERSTORM (int): `95`
+        SEVERE_THUNDERSTORM (int): `97`
 
     Examples:
         >>> Weather(0)
@@ -40,17 +40,17 @@ class Weather(IntEnum):
         0
     """
 
-    CLEAR_SKIES: int = 0
-    SUNNY: int = 1
-    PARTLY_CLOUDY: int = 2
-    MOSTLY_CLOUDY: int = 3
-    OVERCAST: int = 4
-    HAZE: int = 5
-    SMOKE: int = 10
-    THUNDER: int = 17
-    FOG: int = 45
-    LIGHT_RAIN: int = 61
-    MODERATE_RAIN: int = 63
-    ISOLATED_SHOWER: int = 80
-    THUNDERSTORM: int = 95
-    SEVERE_THUNDERSTORM: int = 97
+    CLEAR_SKIES = 0
+    SUNNY = 1
+    PARTLY_CLOUDY = 2
+    MOSTLY_CLOUDY = 3
+    OVERCAST = 4
+    HAZE = 5
+    SMOKE = 10
+    THUNDER = 17
+    FOG = 45
+    LIGHT_RAIN = 61
+    MODERATE_RAIN = 63
+    ISOLATED_SHOWER = 80
+    THUNDERSTORM = 95
+    SEVERE_THUNDERSTORM = 97
